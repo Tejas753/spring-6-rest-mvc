36a8faae8a1c0f6ab5b1864d9b9fb508d990d7ec
Tejas Sinapanahalli Gangadharaiah <tejas.sg753@gmail.com>
2023-03-30 17:09:39 -0500

    MockMVC - Test ListBeers

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -35,6 +35,16 @@ class BeerControllerTest {
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
 
     @Test
+    void testListBeers() throws Exception {
+        given(beerService.beerList()).willReturn(beerServiceImpl.beerList());
+
+        mockMvc.perform(get("/api/v1/beer").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+    }
+
+    @Test
     void getBeerById() throws Exception {
         Beer testBeer = beerServiceImpl.beerList().get(0);
 
